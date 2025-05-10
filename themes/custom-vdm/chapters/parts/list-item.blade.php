c0e2a15c0f2c230c8202b1b2cac5a42f389d35d9
valen-tino <59046897+valen-tino@users.noreply.github.com>
2025-05-10 15:43:52 +0800

    feat(ui): add chapter and page icons to list items for better visual hierarchy & remove /books url from navbar
    This commit introduces icons for chapters and pages in the list items to improve the visual hierarchy and user experience. The icons are styled with a consistent size and alignment. Additionally, the /books url is removed from navbar & default home page file was removed as it is no longer needed.

--- themes/custom-vdm/chapters/parts/list-item.blade.php
+++ themes/custom-vdm/chapters/parts/list-item.blade.php
@@ -3,7 +3,7 @@
 <a href="{{ $chapter->getUrl() }}" class="chapter entity-list-item @if($chapter->visible_pages->count() > 0) has-children @endif" data-entity-type="chapter" data-entity-id="{{$chapter->id}}">
     <span class="icon text-chapter">@icon('chapter')</span>
     <div class="content">
-        <h4 class="entity-list-item-name break-text">{{ $chapter->name}}</h4>
+        <h4 class="entity-list-item-name break-text"><span style="display: inline-block; vertical-align: middle;">@icon('chapter', ['style' => 'width: 24px; height: 24px;'])</span> {{ $chapter->name}}</h4>
         <div class="entity-item-snippet">
             <p class="text-muted break-text">{{ $chapter->getExcerpt() }}</p>
         </div>
